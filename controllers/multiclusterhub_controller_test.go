aa32bfbfe7808aebd8a33bf46a65eb134fa143ff
Erin Murphy <erinmurp@redhat.com>
2023-06-26 13:47:19 -0400

    cluster-permission operator install

--- controllers/multiclusterhub_controller_test.go
+++ controllers/multiclusterhub_controller_test.go
@@ -647,6 +647,18 @@ var _ = Describe("MultiClusterHub controller", func() {
 			result, err = reconciler.ensureNoVolsync(ctx, mch, testImages)
 			Expect(result).To(Equal(ctrl.Result{}))
 			Expect(err).To(BeNil())
+
+			By("Ensuring ClusterPermission")
+
+			result, err = reconciler.ensureClusterPermission(ctx, mch, testImages)
+			Expect(result).To(Equal(ctrl.Result{}))
+			Expect(err).To(BeNil())
+
+			By("Ensuring No ClusterPermission")
+
+			result, err = reconciler.ensureNoClusterPermission(ctx, mch, testImages)
+			Expect(result).To(Equal(ctrl.Result{}))
+			Expect(err).To(BeNil())
 		})
 	})
 
